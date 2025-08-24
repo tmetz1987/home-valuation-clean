f6c4dec64687545c0426fdc3ae2625bc97795005
tmetz1987 <tylerzmetzger@gmail.com>
2025-08-23 20:39:12 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata = { title: "WA Home Valuation", description: "Estimate your home value (WA-only beta)" };
+export default function RootLayout({ children }) { return <html lang="en"><body>{children}</body></html>; }
